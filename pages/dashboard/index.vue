d7438623135735cadfc7b3a2ffd9a1a1aae963bd
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2023-12-03 23:06:21 -0300

    Correções em testes

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <h1>Dashboard</h1>
+                    <h1>Sistema Agricultura</h1>
                 </div>
             </div>
         </div>
--- tests/test-produtos.spec.ts
+++ tests/test-produtos.spec.ts
@@ -6,17 +6,16 @@ describe("Teste de produtos", async () => {
   await setup();
 
   test("Pode criar produto", async () => {
-
     const produto = {
-        codigo: faker.number.int({ min: 100000, max: 999999999999 }).toString(),
-        nome: faker.commerce.productName(),
-        preco: faker.commerce.price(),
-        quantidade: faker.number.int({ min: 1, max: 100 }),
-        descricao: faker.commerce.productDescription(),
-        fornecedor_id: 1,
-        data_validade: faker.date.future(),
-        numero_pedido: faker.number.int({ min: 1, max: 100 }),
-        lote: faker.number.int({ min: 1, max: 100 })
+      codigo: faker.number.int({ min: 100000, max: 999999999999 }).toString(),
+      nome: faker.commerce.productName(),
+      preco: faker.number.float({ min: 1, max: 100 }),
+      quantidade: faker.number.int({ min: 1, max: 100 }),
+      descricao: faker.commerce.productDescription(),
+      fornecedor_id: 1,
+      data_validade: faker.date.future(),
+      numero_pedido: faker.number.int({ min: 1, max: 100 }).toString(),
+      lote: faker.number.int({ min: 1, max: 100 }).toString(),
     };
 
     const response = await $fetch("/api/v1/produtos/create", {
@@ -27,86 +26,79 @@ describe("Teste de produtos", async () => {
     expect(response.statusCode).toBe(200);
   });
 
-    test("Pode listar produtos", async () => {
-        const response = await $fetch("/api/v1/produtos");
-        expect(response.statusCode).toBe(200);
-    
-        expect(response).toHaveProperty("data");
+  test("Pode listar produtos", async () => {
+    const response = await $fetch("/api/v1/produtos");
+    expect(response.statusCode).toBe(200);
+
+    expect(response).toHaveProperty("data");
+  });
+
+  test("Pode Editar produto", async () => {
+    const produto = {
+      codigo: faker.number.int({ min: 100000, max: 999999999999 }).toString(),
+      nome: faker.commerce.productName(),
+      preco: faker.number.float({ min: 1, max: 100 }),
+      quantidade: faker.number.int({ min: 1, max: 100 }),
+      descricao: faker.commerce.productDescription(),
+      fornecedor_id: 1,
+      data_validade: faker.date.future(),
+      numero_pedido: faker.number.int({ min: 1, max: 100 }).toString(),
+      lote: faker.number.int({ min: 1, max: 100 }).toString(),
+    };
+
+    const response = await $fetch("/api/v1/produtos/create", {
+      method: "POST",
+      body: JSON.stringify(produto),
     });
 
-    test("Pode Editar produto", async () => {
-        const produto = {
-            codigo: faker.number.int({ min: 100000, max: 999999999999 }).toString(),
-            nome: faker.commerce.productName(),
-            preco: faker.commerce.price(),
-            quantidade: faker.number.int({ min: 1, max: 100 }),
-            descricao: faker.commerce.productDescription(),
-            fornecedor_id: 1,
-            data_validade: faker.date.future(),
-            numero_pedido: faker.number.int({ min: 1, max: 100 }),
-            lote: faker.number.int({ min: 1, max: 100 })
-        };
+    expect(response.statusCode).toBe(200);
 
-        const response = await $fetch("/api/v1/produtos/create", {
-            method: "POST",
-            body: JSON.stringify(produto),
-        });
-
-        expect(response.statusCode).toBe(200);
-
-        const responseEdit = await $fetch(
-            `/api/v1/produtos/update`,
-            {
-                method: "PUT",
-                body: JSON.stringify({
-                    id: response.data.id,
-                    codigo: faker.number.int({ min: 100000, max: 999999999999 }).toString(),
-                    nome: faker.commerce.productName(),
-                    preco: faker.commerce.price(),
-                    quantidade: faker.number.int({ min: 1, max: 100 }),
-                    descricao: faker.commerce.productDescription(),
-                    fornecedor_id: 1,
-                    data_validade: faker.date.future(),
-                    numero_pedido: faker.number.int({ min: 1, max: 100 }),
-                    lote: faker.number.int({ min: 1, max: 100 })
-                }),
-            }
-        );
-
-        expect(responseEdit.statusCode).toBe(200);
+    const responseEdit = await $fetch(`/api/v1/produtos/update`, {
+      method: "PUT",
+      body: JSON.stringify({
+        id: response.data.id,
+        codigo: faker.number.int({ min: 100000, max: 999999999999 }).toString(),
+        nome: faker.commerce.productName(),
+        preco: faker.number.float({ min: 1, max: 100 }),
+        quantidade: faker.number.int({ min: 1, max: 100 }),
+        descricao: faker.commerce.productDescription(),
+        fornecedor_id: 1,
+        data_validade: faker.date.future(),
+        numero_pedido: faker.number.int({ min: 1, max: 100 }).toString(),
+        lote: faker.number.int({ min: 1, max: 100 }).toString(),
+      }),
     });
 
-    test("Pode deletar produto", async () => {
-        const produto = {
-            codigo: faker.number.int({ min: 100000, max: 999999999999 }).toString(),
-            nome: faker.commerce.productName(),
-            preco: faker.commerce.price(),
-            quantidade: faker.number.int({ min: 1, max: 100 }),
-            descricao: faker.commerce.productDescription(),
-            fornecedor_id: 1,
-            data_validade: faker.date.future(),
-            numero_pedido: faker.number.int({ min: 1, max: 100 }),
-            lote: faker.number.int({ min: 1, max: 100 })
-        };
+    expect(responseEdit.statusCode).toBe(200);
+  });
 
-        const response = await $fetch("/api/v1/produtos/create", {
-            method: "POST",
-            body: JSON.stringify(produto),
-        });
+  test("Pode deletar produto", async () => {
+    const produto = {
+      codigo: faker.number.int({ min: 100000, max: 999999999999 }).toString(),
+      nome: faker.commerce.productName(),
+      preco: faker.number.float({ min: 1, max: 100 }),
+      quantidade: faker.number.int({ min: 1, max: 100 }),
+      descricao: faker.commerce.productDescription(),
+      fornecedor_id: 1,
+      data_validade: faker.date.future(),
+      numero_pedido: faker.number.int({ min: 1, max: 100 }).toString(),
+      lote: faker.number.int({ min: 1, max: 100 }).toString(),
+    };
 
-        expect(response.statusCode).toBe(200);
-
-        const responseDelete = await $fetch(
-            `/api/v1/produtos/delete`,
-            {
-                method: "DELETE",
-                body: JSON.stringify({
-                    id: response.data.id,
-                }),
-            }
-        );
-
-        expect(responseDelete.statusCode).toBe(200);
-
+    const response = await $fetch("/api/v1/produtos/create", {
+      method: "POST",
+      body: JSON.stringify(produto),
     });
+
+    expect(response.statusCode).toBe(200);
+
+    const responseDelete = await $fetch(
+      `/api/v1/produtos/${response.data.id}`,
+      {
+        method: "DELETE",
+      }
+    );
+
+    expect(responseDelete.statusCode).toBe(200);
+  });
 });
